eaa6ad95d35418af1bae92241806004dcb748e01
Rathul P <rathulp2@gmail.com>
2024-07-25 12:25:23 +0530

    Edit functionality Added

--- Controllers/EmployesController.cs
+++ Controllers/EmployesController.cs
@@ -33,5 +33,29 @@ namespace Employee.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Employe employedata = empDbContext.employees.Find(id);
+            return View(employedata);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employe employeupdateddata) {
+            Employe existemploye = empDbContext.employees.Find(employeupdateddata.Id);
+            if (existemploye != null) {
+                existemploye.Name = employeupdateddata.Name;
+                existemploye.Phone = employeupdateddata.Phone;
+                existemploye.Email = employeupdateddata.Email;
+                empDbContext.SaveChanges();
+                TempData["AlertMessage"] = "Employe Updated Sucessfull...";
+            }
+            else
+            {
+                TempData["AlertMessage"] = "Some Think Went To Wrong...";
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
