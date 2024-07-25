6c72334e9bc685d1993cac09ac687365cd05a2ea
Rathul P <rathulp2@gmail.com>
2024-07-25 10:06:43 +0530

    Employe controller created and add employe functionality added

---
+++ Controllers/EmployesController.cs
@@ -0,0 +1,36 @@
+﻿using Employee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Employee.Controllers
+{
+    public class EmployesController : Controller
+    {
+
+        private EmployeEntity empDbContext = new EmployeEntity();
+        // GET: Employes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddEmploye()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEmploye(Employe emp)
+        {
+            if (ModelState.IsValid) {
+                empDbContext.employees.Add(emp);
+                empDbContext.SaveChanges();
+                TempData["AlertMessage"] = "Employe Added Sucessfull...";
+            }
+            return View();
+        }
+    }
+}
